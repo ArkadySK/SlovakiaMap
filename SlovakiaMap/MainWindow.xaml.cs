ebee8fa4f49903e638fded69117fcee36af13c83
ArkadySK <adamkooo2@gmail.com>
2023-06-12 21:24:43 +0200

    Moved SlovakiaViewModel into ViewModels

--- SlovakiaMap/MainWindow.xaml.cs
+++ SlovakiaMap/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Shapes;
+using SlovakiaMap.ViewModels;
 
 namespace SlovakiaMap
 {
---
+++ SlovakiaMap/ViewModels/SlovakiaViewModel.cs
@@ -0,0 +1,90 @@
+﻿using SlovakiaMap.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SlovakiaMap.ViewModels
+{
+    public class SlovakiaViewModel : INotifyPropertyChanged
+    {
+        private District? selectedDistrict;
+
+        private readonly Dictionary<string, District> districts = new();
+
+        public Dictionary<string, District> Municipatilies
+        {
+            get => districts;
+        }
+        public District? SelectedMunicipality
+        {
+            get => selectedDistrict;
+            set
+            {
+                selectedDistrict = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        internal void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task LoadDistricts(string filePath)
+        {
+            Municipatilies.Clear();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var file = new StreamReader(filePath, Encoding.GetEncoding("windows-1250"), true);
+
+            //var color = Colors.Gray;
+            string data = await file.ReadToEndAsync();
+            foreach (string line in data.Split("\n"))
+            {
+                try
+                {
+                    CreateDistrict(line);
+                }
+                catch { }
+            }
+            file.Close();
+            await Task.CompletedTask;
+
+            SortMunicipalitiesByResidents();
+        }
+
+        private void SortMunicipalitiesByResidents()
+        {
+            var color = Color.FromArgb(255, 50, 50, 50);
+
+            List<District> districtsList = districts.Values.ToList();
+            districtsList = (from m in districtsList
+                             orderby m.ResidentsCount ascending
+                             select m)
+                                .ToList();
+
+            foreach (District muni in districtsList)
+            {
+                color = Color.FromArgb(color.A, (byte)(color.R + 2), color.G, color.B);
+                Brush brush = new SolidColorBrush(color);
+                muni.Fill = brush;
+            }
+        }
+
+        private void CreateDistrict(string lineInfo)
+        {
+            var infos = lineInfo.Split(";");
+            var name = infos[0];
+            var spz = infos[1];
+            var kraj = infos[3];
+            int obyv = int.Parse(infos[4].Replace(" ", ""));
+            Municipatilies.Add(spz, new District(name, kraj, spz, obyv));
+        }
+    }
+}
