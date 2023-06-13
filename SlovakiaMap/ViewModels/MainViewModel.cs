9967cbc0beadd216e6f8663b46b8de67c0f55e6f
ArkadySK <adamkooo2@gmail.com>
2023-06-13 23:16:40 +0200

    reworked viewmodel, updated bindings

--- SlovakiaMap/ViewModels/MainViewModel.cs
+++ SlovakiaMap/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SlovakiaMap.Models;
+﻿using CommunityToolkit.Mvvm.Input;
+using SlovakiaMap.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -13,17 +14,38 @@ namespace SlovakiaMap.ViewModels
 {
     public class MainViewModel
     {
-        // TODO: implement ICommand to a class
-        public ICommand AreaCommand { get; set; }
-        public ICommand ResidentsCountCommand { get; set; }
-        public ICommand ResidentsDensityCommand { get; set; }
+        public ICommand AreaCommand { get; }
+        public ICommand ResidentsCountCommand { get; }
+        public ICommand ResidentsDensityCommand { get; }
 
-        public SlovakiaViewModel Slovakia { get; set; } //TODO: get only?
+        public SlovakiaViewModel Slovakia { get; } //TODO: get only?
+
+        public MainViewModel()
+        {
+            AreaCommand = new RelayCommand(Area);
+            ResidentsCountCommand = new RelayCommand(ResidentsCount);
+            ResidentsDensityCommand = new RelayCommand(ResidentsDensity);
+            Slovakia = new SlovakiaViewModel();
+        }
 
         public async Task Load(string filePath)
         {
-            Slovakia = new SlovakiaViewModel();
             await Slovakia.LoadDistricts(filePath);
+        }
+
+        private void Area()
+        {
+
+        }
+
+        private void ResidentsCount()
+        {
+
+        }
+
+        private void ResidentsDensity()
+        {
+
         }
     }
 }
