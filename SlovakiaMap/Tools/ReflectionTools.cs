7efa8b89c8c71e8182c66e030e90b3725433346d
adam majerik <adamkooo2@gmail.com>
2023-06-16 11:47:12 +0200

    added tools folder and reflection tools class

---
+++ SlovakiaMap/Tools/ReflectionTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlovakiaMap.Tools
+{
+    public class ReflectionTools
+    {
+        public static object GetPropValue(object src, string propName)
+        {
+            if (src is null || string.IsNullOrEmpty(propName))
+                throw new ArgumentException("source object or property name is wrong!");
+
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    }
+}
